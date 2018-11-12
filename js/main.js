c75d9cfb4561adea425cb6af8193d30276b8a728
Kelvin Loh <dotcal604@me.com>
2018-11-11 22:16:10 -0800

    Add menu to index.html

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 console.log('Hello');
 let x = 9;
+
+$.ajax({
+    method: 'GET',
+    url: 'http://api.nytimes.com/svc/topstories/v2/{section}.{response-format}?api-key=14bcc5d04435458ba00c5d1853898e15'
+})
