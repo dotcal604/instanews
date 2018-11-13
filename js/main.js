4eec273c3e2b262cf78f761a57dc5691140ebbb7
Kelvin Loh <dotcal604@me.com>
2018-11-12 19:25:06 -0800

    Add AJAX request.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-console.log('Hello');
-let x = 9;
+"use strict";
 
-$.ajax({
-    method: 'GET',
-    url: 'http://api.nytimes.com/svc/topstories/v2/{section}.{response-format}?api-key=14bcc5d04435458ba00c5d1853898e15'
-})
+$(function() {
+  $("#topics").change(function() {
+    let topic = $(this).find("option:selected");
+    let section = topic.attr("value");
+    console.log(section);
+
+    var url = "https://api.nytimes.com/svc/topstories/v2/{section}.json";
+    url +=
+      "?" +
+      $.param({
+        "api-key": "14bcc5d04435458ba00c5d1853898e15"
+      });
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+      .done(function(result) {
+        console.log(result);
+      })
+      .fail(function(err) {
+        throw err;
+      });
+  });
+});
