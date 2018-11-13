68e9f82ff412db3d763450e8a50f2f7be8118c7e
Kelvin Loh <dotcal604@me.com>
2018-11-12 20:07:19 -0800

    Fix AJAX request.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 "use strict";
 
 $(function() {
+    
   $("#topics").change(function() {
-    let topic = $(this).find("option:selected");
-    let section = topic.attr("value");
+    var section = $('option:selected').attr('value')
     console.log(section);
 
-    var url = "https://api.nytimes.com/svc/topstories/v2/{section}.json";
+    var url = "https://api.nytimes.com/svc/topstories/v2/"+section+".json";
     url +=
       "?" +
       $.param({
         "api-key": "14bcc5d04435458ba00c5d1853898e15"
       });
+
     $.ajax({
       url: url,
       method: "GET"
