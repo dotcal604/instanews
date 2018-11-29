a81b4b965e9f7ae70ec03f40cf03ec9fe97cf966
Kelvin Loh <dotcal604@me.com>
2018-11-28 18:39:02 -0800

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require("gulp"),
-    terser = require("gulp-terser"),
-    rename = require("gulp-rename"),
-    babel = require("gulp-babel"),
+const gulp = require('gulp'),
+    terser = require('gulp-terser'),
+    rename = require('gulp-rename'),
+    babel = require('gulp-babel'),
     bsync = require('browser-sync'),
     eslint = require('gulp-eslint'),
-    sass = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer"),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
     prettyError = require('gulp-prettyerror');
     
@@ -13,39 +13,39 @@ gulp.task('scripts', function(){
   return gulp.src('./js/main.js')
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(terser())
     .pipe(babel())
+    .pipe(terser())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task("styles", function() {
+gulp.task('styles', function() {
     return gulp
-      .src("./sass/style.scss")
+      .src('./sass/style.scss')
       .pipe(prettyError()) 
       .pipe(sass())
       .pipe(
         autoprefixer({
-          browsers: ["last 2 versions"]
+          browsers: ['last 2 versions']
         })
       )
-      .pipe(gulp.dest("./build/css"))
+      .pipe(gulp.dest('./build/css'))
       .pipe(cssnano())
-      .pipe(rename("style.min.css"))
-      .pipe(gulp.dest("./build/css"));
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
   });
 
 
 gulp.task('bwatch', function () {
     bsync.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         }
     });
 
     gulp
-    .watch(["*.html", "js/*.js", "sass/*.scss"])
-    .on("change", bsync.reload);
+    .watch(['*.html', 'js/*.js', 'sass/*.scss'])
+    .on('change', bsync.reload);
 });
 
 gulp.task('default', gulp.series('scripts', 'styles', 'bwatch'));
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let x = 5;
+
 $(function() {
     
   $("#topics").change(function() {
